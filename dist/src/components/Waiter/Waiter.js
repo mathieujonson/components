8ad3ca60f1028ed6fa4bb4003bfbc57ec7ee8672
Matthew Johnson <matthewjohnson@a-xrc7jjg5j.carfax.cfx>
2019-08-08 19:50:36 -0500

    tags Waiter

---
+++ dist/src/components/Waiter/Waiter.js
@@ -0,0 +1,22 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+require("./waiter.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _default = function _default() {
+  return _react.default.createElement("div", {
+    class: "waiter"
+  });
+};
+
+exports.default = _default;
+
+//# sourceMappingURL=Waiter.js.map

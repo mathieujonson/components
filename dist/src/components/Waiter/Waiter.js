8bc2cb17515ecad06d0de3d993a32d41ac3bf75f
Matthew Johnson <matthewjohnson@a-xrc7jjg5j.carfax.cfx>
2019-08-08 23:08:52 -0500

    reverts naming

--- dist/src/components/Waiter/Waiter.js
+++ dist/src/components/Waiter/Waiter.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = void 0;
+exports.Waiter = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
@@ -11,12 +11,12 @@ require("./waiter.css");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _default = function _default() {
+var Waiter = function Waiter() {
   return _react.default.createElement("div", {
     class: "waiter"
   });
 };
 
-exports.default = _default;
+exports.Waiter = Waiter;
 
 //# sourceMappingURL=Waiter.js.map
